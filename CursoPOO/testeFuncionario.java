7b4a0ec2efaa593e52157198123b20a4f41ec9de
16Lucas <94024150+16Lucas@users.noreply.github.com>
2024-09-23 19:22:58 -0300

    Add files via upload

---
+++ CursoPOO/testeFuncionario.java
@@ -0,0 +1,7 @@
+public class testeFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario("Lucas", 24, 4500.00);
+
+        funcionario1.info();
+    }
+}
